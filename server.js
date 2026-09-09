66d456eb92736171e48858b172700f4ace02af79
alicemartins-hue <alicemartins7l@hotmail.com>
2026-09-09 13:27:40 -0300

    feat: add URL and URLSearchParams implementations and utilities
    - Introduced `urlencoded.js` for parsing and serializing URL-encoded data.
    - Added `utils.js` for various utility functions including object handling and property definitions.
    - Created `package.json` for the whatwg-url package, specifying dependencies and scripts.
    - Implemented `webidl2js-wrapper.js` to export URL and URLSearchParams modules.

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 const express = require('express');
 const app = express();
+const mongodb = require('./data/database');
 
 const port = process.env.PORT || 3000;
 
-app.use('/',require('./routes'));
-app.listen(port, () => {console.log(`Running in port ${port}`)});
+app.use('/', require('./routes'));
+
+
+
+
+
+
+mongodb.initDB((err) => {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and node Running on port ${port}`)
+        });
+    };
+});
